70c3030c2b0db879156758987c4d7c6069b0880b
vinix997 <chandra.delon@binus.ac.id>
2022-08-07 22:18:26 +0700

    Tidy up main.go

--- main.go
+++ main.go
@@ -20,37 +20,49 @@ func main() {
 			fmt.Println("Recovered. Error: ", r)
 		}
 	}()
+	//Connection binding
 	conn := Connection()
+
+	//Repository
 	userRepo := repository.NewUserRepository(conn)
 	photoRepo := repository.NewPhotoRepository(conn)
 	commentRepo := repository.NewCommentRepository(conn)
 	socialRepo := repository.NewSocialRepository(conn)
 
+	//Service
 	userService := service.NewUserSvc(userRepo)
 	photoService := service.NewPhotoSvc(photoRepo)
 	commentService := service.NewCommentSvc(commentRepo)
 	socialService := service.NewSocialSvc(socialRepo)
 
+	//Handler
 	userHandler := handler.NewUserHandler(userService, conn)
 	photoHandler := handler.NewPhotoHandler(photoService, conn)
 	commentHandler := handler.NewCommentHandler(commentService, conn)
 	socialHandler := handler.NewSocialHandler(socialService, conn)
+
 	authMiddleware := middleware.NewAuthMiddleware()
 
+	//Endpoints
 	r := mux.NewRouter()
 	userRoutes := r.PathPrefix("/users").Subrouter()
 	userRoutes.HandleFunc("/register", userHandler.UserHandler)
 	userRoutes.HandleFunc("/login", userHandler.UserHandler)
 	userRoutes.HandleFunc("/{userId}", userHandler.UserHandler)
+
 	photoRoutes := r.PathPrefix("/photos").Subrouter()
 	photoRoutes.HandleFunc("", photoHandler.PhotoHandler)
 	photoRoutes.HandleFunc("/{photoId}", photoHandler.PhotoHandler)
+
 	commentRoutes := r.PathPrefix("/comments").Subrouter()
 	commentRoutes.HandleFunc("/{commentId}", commentHandler.CommentHandler)
 	commentRoutes.HandleFunc("", commentHandler.CommentHandler)
+
 	socialRoutes := r.PathPrefix("/socialmedias").Subrouter()
 	socialRoutes.HandleFunc("", socialHandler.SocialHandler)
 	socialRoutes.HandleFunc("/{socialMediaId}", socialHandler.SocialHandler)
+
+	//Middleware
 	userRoutes.Use(authMiddleware.AuthMiddleware)
 	photoRoutes.Use(authMiddleware.AuthMiddleware)
 	commentRoutes.Use(authMiddleware.AuthMiddleware)
